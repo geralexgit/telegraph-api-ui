362871444f30280249d0f8fe9763f2d44148abab
geralexgit <alexandgeras@gmail.com>
2016-12-25 17:39:26 +0300

    keeps access_token in local storage

--- app/TerminalApp.js
+++ app/TerminalApp.js
@@ -1,12 +1,13 @@
 var terminalApp = angular.module('TerminalApp', []);
 
-terminalApp.controller('MainController', ['$scope', '$http', '$httpParamSerializerJQLike', function ($scope, $http, $httpParamSerializerJQLike) {
+terminalApp.controller('MainController', ['$scope', '$http', '$httpParamSerializerJQLike', function($scope, $http, $httpParamSerializerJQLike) {
     $scope.createUserFormIsShown = true;
     $scope.editUserFormIsShown = false;
     $scope.createUserData = {};
-
+    $scope.user = {};
+    $scope.access_token = JSON.parse(localStorage.getItem('token')) || {};
     //create user
-    $scope.createUser = function () {
+    $scope.createUser = function() {
         console.log('Hi');
         console.log($scope.createUserData);
         $http({
@@ -16,9 +17,25 @@ terminalApp.controller('MainController', ['$scope', '$http', '$httpParamSerializ
             headers: {
                 'Content-Type': 'application/x-www-form-urlencoded'
             }
-        }).then(function (res) {
-            console.log(res);
-        }, function (error) {
+        }).then(function(response) {
+            console.log(response);
+            /*{ "ok": true,
+                "result": {
+                    "short_name": "myName",
+                    "author_name": "superAuthor",
+                    "author_url": "http://link.com/",
+                    "access_token": "fc2f6e55ce85955b9684f2f7ef97126cff96a1f68f029cba496c8097b4be",
+                    "auth_url": "https://edit.telegra.ph/auth/YIIv6Iwj2swHC9y7YLf4k5xoLYqWEWYCCG5ImB1o2v"
+            }}*/
+            if (response.data.ok === true) {
+                $scope.user = response.data.result;
+                $scope.access_token = $scope.user.access_token;
+                localStorage.setItem('access_token', JSON.stringify($scope.user.access_token));
+            } else {
+                console.log('response.data.error');
+            }
+
+        }, function(error) {
             console.log(error);
         });
     };
