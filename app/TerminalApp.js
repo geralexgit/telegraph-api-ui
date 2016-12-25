e9a740ac1fb2cc4764d914f80f8c654bc63f7366
geralexgit <alexandgeras@gmail.com>
2016-12-25 17:05:52 +0300

    create user sends form

--- app/TerminalApp.js
+++ app/TerminalApp.js
@@ -1,35 +1,25 @@
 var terminalApp = angular.module('TerminalApp', []);
 
-terminalApp.controller('MainController', ['$scope', function($scope) {
-
+terminalApp.controller('MainController', ['$scope', '$http', '$httpParamSerializerJQLike', function ($scope, $http, $httpParamSerializerJQLike) {
     $scope.createUserFormIsShown = true;
     $scope.editUserFormIsShown = false;
     $scope.createUserData = {};
 
-    $scope.createUser = function($scope, $http) {
-        console.log('Ji');
+    //create user
+    $scope.createUser = function () {
+        console.log('Hi');
         console.log($scope.createUserData);
-
-        /*$http({
-                method: 'POST',
-                url: 'https://api.telegra.ph/createAccount',
-                data: $scope.createUserData, // pass in data as strings
-                headers: { 'Content-Type': 'application/x-www-form-urlencoded' } // set the headers so angular passing info as form data (not request payload)
-            })
-            .success(function(data) {
-
-                console.log(data);
-
-                if (!data.success) {
-                    // if not successful, bind errors to error variables
-                    $scope.errorName = data.errors.name;
-                    $scope.errorSuperhero = data.errors.superheroAlias;
-                } else {
-                    // if successful, bind success message to message
-                    $scope.message = data.message;
-                }
-
-            });*/
-            
+        $http({
+            url: 'https://api.telegra.ph/createAccount',
+            method: 'POST',
+            data: $httpParamSerializerJQLike($scope.createUserData),
+            headers: {
+                'Content-Type': 'application/x-www-form-urlencoded'
+            }
+        }).then(function (res) {
+            console.log(res);
+        }, function (error) {
+            console.log(error);
+        });
     };
 }]);
