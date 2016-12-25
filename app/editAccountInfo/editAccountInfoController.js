e8338aaa0e62ba106c4433c88dddc73468df6495
geralexgit <alexandgeras@gmail.com>
2016-12-25 22:47:23 +0300

    getUserInfo in progress

--- app/editAccountInfo/editAccountInfoController.js
+++ app/editAccountInfo/editAccountInfoController.js
@@ -12,12 +12,39 @@ terminalApp.controller('editAccountInfoController', ['$scope', '$http', '$httpPa
         author_name: $scope.author_name,
         author_url: $scope.author_url
     };
-    
+
     $scope.textEdit = false;
-    
-    
-    $scope.getAccountInfo = function(){
+
+
+    $scope.getAccountInfo = function () {
         console.log($scope.access_token);
+
+        var settings = ['short_name', 'author_name', 'author_url'];
+
+        $scope.getUserInfo = {
+            access_token: $scope.userInfo.access_token,
+            fields: settings.toString()
+        };
+
+        $scope.params = $httpParamSerializerJQLike($scope.getUserInfo);
+        console.log($scope.getUserInfo);
+        console.log($scope.params);
+        /*
+         $http({
+         url: 'https://api.telegra.ph/getAccountInfo',
+         method: 'POST',
+         data: $httpParamSerializerJQLike($scope.getUserInfo),
+         headers: {
+         'Content-Type': 'application/x-www-form-urlencoded'
+         }
+         }).then(function (response) {
+         console.log(response);
+         if (response.data.ok === true) {
+         } else {
+         }
+         }, function (error) {
+         console.log(error);
+         });*/
     };
 
 }]);
