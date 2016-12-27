acf2a9bc3e0dfcc4af1e05f4d68bf21946cc3cd6
geralexgit <alexandgeras@gmail.com>
2016-12-27 22:21:49 +0300

    createPage small update

--- app/createPage/createPageController.js
+++ app/createPage/createPageController.js
@@ -1,4 +1,7 @@
 terminalApp.controller('createPageController', ['$scope', '$http', '$httpParamSerializerJQLike', 'DataService',
     function ($scope, $http, $httpParamSerializerJQLike, DataService) {
+        $scope.pageText = '';
         $scope.accountInfo = DataService.accountInfo;
+        $scope.pageContent = angular.copy($scope.accountInfo);
+        $scope.pageContent.content = [{"tag":"p","children":$scope.pageText}];
     }]);
