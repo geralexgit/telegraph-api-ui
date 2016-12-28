e22cf50277daca6370ac5f6c652333e678f14a1a
geralexgit <alexandgeras@gmail.com>
2016-12-29 00:14:25 +0300

    createPage still dosen't work

--- app/createPage/createPageController.js
+++ app/createPage/createPageController.js
@@ -1,7 +1,67 @@
 terminalApp.controller('createPageController', ['$scope', '$http', '$httpParamSerializerJQLike', 'DataService',
     function ($scope, $http, $httpParamSerializerJQLike, DataService) {
+
+        var url = 'https://api.telegra.ph/createPage/';
+
         $scope.pageText = '';
+
         $scope.accountInfo = DataService.accountInfo;
-        $scope.pageContent = angular.copy($scope.accountInfo);
-        $scope.pageContent.content = [{"tag":"p","children":$scope.pageText}];
+
+        $scope.pageContent = {};
+
+        $scope.$watch('pageText', function (value) {
+            $scope.pageContent.content = [{"tag": "p", "children": value}];
+        });
+
+        /*$scope.createPage = function () {
+
+
+         $http.post(url, {params: data}).then(function (response) {
+         if (response.data.ok === true) {
+         console.log('Everything is fine');
+         console.log(response.data.result);
+         $scope.response = response.data.result;
+         } else {
+         console.log('Something went wrong');
+         }
+         })
+         }*/
+
+
+        $scope.createPage = function () {
+
+            $scope.data = {
+                access_token: $scope.accountInfo.access_token,
+                title: $scope.pageContent.title,
+                author_name: $scope.accountInfo.author_name,
+                author_url: $scope.accountInfo.author_url,
+                content: $scope.pageContent.content,
+                return_content: $scope.pageContent.return_content
+            };
+
+            $http({
+                url: url,
+                method: 'POST',
+                data: $httpParamSerializerJQLike($scope.data),
+                headers: {
+                    'Content-Type': 'application/x-www-form-urlencoded'
+                }
+            }).then(function (response) {
+                if (response.data.ok === true) {
+                    console.log('Everything is fine');
+                    console.log(response.data.result);
+                    $scope.response = response.data.result;
+                } else {
+
+                    console.log('Something went wrong');
+                    console.log(response);
+                }
+            });
+        };
+
     }]);
+
+
+/*
+ */
+
---
+++ app/createPage/response.data.js
@@ -0,0 +1,22 @@
+/*
+{
+    "ok": true,
+    "result": {
+    "path": "Sample-Page-12-28-2",
+        "url": "http://telegra.ph/Sample-Page-12-28-2",
+        "title": "Sample Page",
+        "description": "",
+        "author_name": "Anonymous",
+        "content": [
+        {
+            "tag": "p",
+            "children": [
+                "Hello, world!"
+            ]
+        }
+    ],
+        "views": 0,
+        "can_edit": true
+}
+}
+*/
